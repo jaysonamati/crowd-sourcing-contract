c4c8e745bcae51e67227eafb4343c5fd723dc5ea
jaysonamati <jaysonamati@gmail.com>
2023-06-12 17:59:52 +0300

    added the basic code for the funding action

--- src/Kuza/OnChain/KuzaProjects.hs
+++ src/Kuza/OnChain/KuzaProjects.hs
@@ -6,6 +6,7 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE NumericUnderscores #-}
 
 {-# OPTIONS_GHC -fno-specialise #-}
 {-# OPTIONS_GHC -fno-strictness #-}
@@ -19,41 +20,30 @@ module Kuza.OnChain.KuzaProjects
     , saveLucidCode
     ) where
 
-import           Control.Lens                hiding (contains, to)
+import           Control.Lens                hiding (contains, to, from)
 -- import qualified Data.ByteString.Lazy        as LB
 -- import qualified Data.ByteString.Short       as SBS
 -- import           Codec.Serialise             ( serialise )
 import           Prelude                     (Show (..), IO)
-import           Prelude                     hiding (Bool, (.), ($))
+import           Prelude                     hiding (Bool, (.), ($), (&&), foldl, map, mconcat, any, elem, (>=), (<=), not)
 
 
 import           PlutusTx                  ( compile, applyCode, unstableMakeIsData, FromData(fromBuiltinData), makeLift, liftCode, CompiledCode )
 import           PlutusTx.Prelude            hiding (Semigroup(..), unless)
-import           PlutusTx.Prelude            (Bool, BuiltinData, (.), ($))
-import           Plutus.V1.Ledger.Value       ( TokenName, geq, AssetClass)   
+import           PlutusTx.Prelude            (Bool, BuiltinData, (.), ($), (&&))
+import           Plutus.V1.Ledger.Value       ( TokenName, geq, AssetClass, gt, singleton, adaToken, adaSymbol, isZero)
 import qualified Plutus.V1.Ledger.Value    as Value
+import           Plutus.V1.Ledger.Interval (contains)
 -- import qualified Plutus.V1.Ledger.Api      as Ledger
 -- import           Plutus.V1.Ledger.Address     (scriptHashAddress)
 import           Plutus.V2.Ledger.Contexts as V2
-import           Plutus.V2.Ledger.Api         ( mkValidatorScript, Validator, MintingPolicyHash, Address, PubKeyHash, Validator, ScriptContext,
+import           Plutus.V2.Ledger.Api         ( mkValidatorScript, to, Validator, PubKeyHash, Validator, Value,
                                                 POSIXTime, UnsafeFromData (unsafeFromBuiltinData), OutputDatum(OutputDatumHash, NoOutputDatum, OutputDatum),
-                                                Datum(Datum))
+                                                Datum(Datum), CurrencySymbol)
 
 import qualified Utilities.Serialise       as Serialise
-import           Utilities.PlutusTx           (wrapValidator) 
+import           Utilities.PlutusTx           (wrapValidator)
 
-
----------------------------------------------------------------------------------------------------
------------------------------ ON-CHAIN: HELPER FUNCTIONS/TYPES ------------------------------------
-
-{-# INLINABLE parseProjectDatum #-}
-parseProjectDatum :: TxOut -> TxInfo -> Maybe PubKeyHash
-parseProjectDatum o info = case txOutDatum o of 
-    NoOutputDatum -> Nothing
-    OutputDatum (Datum d) -> fromBuiltinData d
-    OutputDatumHash dh -> do 
-                        Datum d <- findDatum dh info
-                        fromBuiltinData d
 
 ---------------------------------------------------------------------------------------------------
 ------------------------------------ ON-CHAIN: VALIDATOR ------------------------------------------
@@ -64,34 +54,99 @@ unstableMakeIsData ''ProjectType
 
 
 data ProjectParams = ProjectParams
-    { projectOwners :: [PubKeyHash]
-    , projectOwnerTokenMintPolicy :: MintingPolicyHash
-    , projectFundingTarget :: Integer
+    { projectFundingTarget :: Integer
     , projectFundingDeadline :: POSIXTime
-    , projectExpenditureTokenMintPolicy :: MintingPolicyHash
     , projectTypeFlag :: ProjectType
     } deriving Show
 makeLift ''ProjectParams
 
-newtype ProjectDatum = ProjectDatum PubKeyHash
+data ProjectDatum = ProjectDatum
+        { spendingMintingPolicyId     :: CurrencySymbol
+        , fundingTokMintingPolicyId   :: CurrencySymbol
+        , votingTokMintingPolicyId    :: CurrencySymbol
+        , projectOwnerTokMintPolicyId :: CurrencySymbol
+        , projectFunders              :: [PubKeyHash]
+        , projectOwners               :: [PubKeyHash]
+        , fundingAmount               :: Integer
+        } deriving Show
 
 unstableMakeIsData ''ProjectDatum
 
 
-data ProjectAction = Vote (PubKeyHash)
+data ProjectAction = Vote PubKeyHash
                    | Fund (AssetClass, Integer, PubKeyHash)
-                   | MoveFunds ()
+                   | MoveFundsProposal PubKeyHash
+                   | MoveFunds (AssetClass, Integer, PubKeyHash)
 
 unstableMakeIsData ''ProjectAction
 
 
 {-# INLINABLE mkProjectsValidator #-}
 mkProjectsValidator :: ProjectParams -> ProjectDatum -> ProjectAction -> ScriptContext -> Bool
-mkProjectsValidator project _ red ctx = True
-    where 
+mkProjectsValidator project dat red ctx =
+    case red of
+        Fund (_, _, pkh)                ->   traceIfFalse "Amount is not greater than one" checkContributionAmount                   &&
+                                             traceIfFalse "Signed By contributor" (signedByContributor pkh)                          &&
+                                             traceIfFalse "Datum has not updated" (checkDatumUpdate pkh)                             &&
+                                             traceIfFalse "Funding token not minted and transferred" (checkFundingTokenTransfer pkh) &&
+                                             traceIfFalse "Deadline has passed" checkDeadlinePassed                                  &&
+                                             traceIfFalse "Target has reached" checkFundingTargetReached
+        MoveFundsProposal _   -> False
+        Vote _                -> False
+        MoveFunds (_, _ ,_)   -> False
+
+
+    where
         info :: TxInfo
         info = scriptContextTxInfo ctx
 
+        checkContributionAmount :: Bool
+        checkContributionAmount = case getContinuingOutputs ctx of
+          [] -> traceError "No outputs paying to the script"
+          os -> let payingOutVal = mconcat $ map txOutValue os
+                in payingOutVal `gt` singleton adaSymbol adaToken 1_000_000
+
+        signedByContributor :: PubKeyHash -> Bool
+        signedByContributor contributorPkh = txSignedBy info contributorPkh
+
+        checkDatumUpdate :: PubKeyHash -> Bool
+        checkDatumUpdate contPkh = case txInfoOutputs info of
+          [] -> traceError "No outputs in the transaction"
+          tos -> any hasFunderPkh tos && any hasIncreasedAmount tos
+            where
+                hasFunderPkh, hasIncreasedAmount:: TxOut -> Bool
+                hasFunderPkh o = case parseProjectDatum o info of
+                                        Nothing -> False
+                                        Just pd -> elem contPkh $ projectFunders pd
+                hasIncreasedAmount o = case parseProjectDatum o info of
+                                        Nothing -> False
+                                        Just pd -> valueProduced info `geq` singleton adaSymbol adaToken (fundingAmount pd)
+
+        checkFundingTokenTransfer :: PubKeyHash -> Bool
+        checkFundingTokenTransfer contrPkh = not (isZero $ valuePaidTo info contrPkh)  -- This is a simple check a more comprehensive check would reference the Currency symbol of the funding mintingPolicy `geq` valueProduced info
+
+        checkDeadlinePassed :: Bool
+        checkDeadlinePassed = contains (to $ projectFundingDeadline project) $ txInfoValidRange info
+
+        checkFundingTargetReached :: Bool
+        checkFundingTargetReached = case txInfoOutputs info of 
+                                            [] -> traceError "No transaction outputs found"
+                                            tout : _ -> case parseProjectDatum tout info of
+                                                                Nothing -> traceError "datum not found"
+                                                                Just pd -> fundingAmount pd <= projectFundingTarget project
+
+
+---------------------------------------------------------------------------------------------------
+----------------------------- ON-CHAIN: HELPER FUNCTIONS/TYPES ------------------------------------
+
+{-# INLINABLE parseProjectDatum #-}
+parseProjectDatum :: TxOut -> TxInfo -> Maybe ProjectDatum
+parseProjectDatum o info = case txOutDatum o of
+    NoOutputDatum -> Nothing
+    OutputDatum (Datum d) -> fromBuiltinData d
+    OutputDatumHash dh -> do
+                        Datum d <- findDatum dh info
+                        fromBuiltinData d
 
 
 ---------------------------------------------------------------------------------------------------
@@ -102,7 +157,7 @@ mkWrappedProjectsValidator :: ProjectParams -> BuiltinData -> BuiltinData -> Bui
 mkWrappedProjectsValidator = wrapValidator . mkProjectsValidator
 
 typedProjectsValidator :: ProjectParams -> Validator
-typedProjectsValidator pp = mkValidatorScript 
+typedProjectsValidator pp = mkValidatorScript
     ($$(compile [|| mkWrappedProjectsValidator ||])
     `applyCode`
     liftCode pp)
@@ -110,37 +165,28 @@ typedProjectsValidator pp = mkValidatorScript
 
 {-# INLINABLE  mkWrappedProjectValidatorLucid #-}
 -- | serializing the projects validator to be used by lucid off-chain
-mkWrappedProjectValidatorLucid :: BuiltinData -- projectOwners
-                               -> BuiltinData -- projectOwnerTokenMintPolicy
-                               -> BuiltinData -- projectFundingTarget
+mkWrappedProjectValidatorLucid :: BuiltinData -- projectFundingTarget
                                -> BuiltinData -- projectFundingDeadline
-                               -> BuiltinData -- projectExpenditureTokenMintPolicy
                                -> BuiltinData -- projectTypeFlag
                                -> BuiltinData -> BuiltinData -> BuiltinData -> () --  datum         redeemer        context
-mkWrappedProjectValidatorLucid po pwtp pft pfd petmp ptf = wrapValidator $ mkProjectsValidator pp 
-    where 
+mkWrappedProjectValidatorLucid pft pfd ptf = wrapValidator $ mkProjectsValidator pp
+    where
         pp = ProjectParams
-            { projectOwners  = unsafeFromBuiltinData po
-            , projectOwnerTokenMintPolicy  = unsafeFromBuiltinData pwtp
-            , projectFundingTarget = unsafeFromBuiltinData pft
+            {projectFundingTarget = unsafeFromBuiltinData pft
             , projectFundingDeadline = unsafeFromBuiltinData pfd
-            , projectExpenditureTokenMintPolicy = unsafeFromBuiltinData petmp
             , projectTypeFlag = unsafeFromBuiltinData ptf
             }
 
-projectsValidatorCodeLucid :: CompiledCode (BuiltinData 
-                                        -> BuiltinData 
-                                        -> BuiltinData 
-                                        -> BuiltinData 
-                                        -> BuiltinData 
-                                        -> BuiltinData 
+projectsValidatorCodeLucid :: CompiledCode (BuiltinData
+                                        -> BuiltinData
+                                        -> BuiltinData
                                         -> BuiltinData -> BuiltinData -> BuiltinData -> ())
-projectsValidatorCodeLucid = $$( compile [|| mkWrappedProjectValidatorLucid ||])                                        
+projectsValidatorCodeLucid = $$( compile [|| mkWrappedProjectValidatorLucid ||])
 
 ------------------------------------------------------------------------------------------------------------------
 ------------------------------------------ HELPER FUNCTIONS ------------------------------------------------------
 
-saveVal :: ProjectParams -> IO () 
+saveVal :: ProjectParams -> IO ()
 saveVal = Serialise.writeValidatorToFile "./assets/kuzaProjects.plutus" . typedProjectsValidator
 
 saveLucidCode :: IO ()
